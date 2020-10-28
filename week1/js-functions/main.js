7c02fc711333634b83cd5d4f4ca156693ac699d2
HealthyJunkie <androwas101@gmail.com>
2020-10-28 19:00:05 +0200

    finished functions excersize

--- week1/js-functions/main.js
+++ week1/js-functions/main.js
@@ -271,3 +271,113 @@ console.log(deleted); //should be [2]
 arr = [1,2,3]
 deleted = arr.splice(1,0,5); 
 console.log(deleted); //should be []
+
+
+
+//Second functions section
+//finish people_info functions:
+const people_info = [
+    {
+      name: "guido",
+      profession: "bungalow builder",
+      age: 17,
+      country: "canaland",
+      city: "sydurn",
+      catchphrase: "what a piece of wood!"
+    },
+    {
+      name: "petra",
+      profession: "jet plane mechanic",
+      age: 31,
+      country: "greenmark",
+      city: "bostork",
+      catchphrase: "that's my engine, bub"
+    },
+    {
+      name: "damian",
+      profession: "nursery assistant",
+      age: 72,
+      country: "zimbia",
+      city: "bekyo",
+      catchphrase: "with great responsibility comes great power"
+    }
+  ]
+
+  const capitalize = function(str){
+    let capitalizedStr = ""
+    capitalizedStr += str[0].toUpperCase(); //add the first letter of the str, capitalized
+    capitalizedStr += str.slice(1) //add the rest of str normallyreturn capitalizedStr
+    return capitalizedStr
+  }
+
+  const capitalizeArray = function(array){
+      let proffesionC = ""
+      for(let i in array){
+          proffesionC += `${capitalize(array[i])} `
+      }
+      return proffesionC
+  }
+
+  const getAge = function(age){
+        if(age < 21)
+            age = "underage"
+        else if(age > 55)
+            age = "55+"
+        else
+            age = age+" years old"
+        return age
+  }
+  const getOrigin = function(person){
+        return `from ${person.city}, ${person.country}.`
+  }
+  
+
+  const getSummary = function(person){
+    let summary = ""
+    summary += capitalize(person.name)
+    summary += ` is ${getAge(person.age)} ` //Yes - you can put a function call inside the tick quotes!
+    summary += `${capitalizeArray(person.profession.split(" "))}`
+    summary += `${getOrigin(person)}`
+    summary += ` ${capitalize(person.name)} loves to say "${capitalize(person.catchphrase)}"`
+    return summary
+  }
+
+  
+  for(let person of people_info){
+    console.log(getSummary(person))
+  }
+  
+//second excersize 
+  const story = "In the beginning there was light. Then there were wolves. Finally there was a big fire. Ultimately, Shelob the wolf-master put out the fire with her feet. But until then, the fire caused one heck of a lot of damage."
+const specialChars = [",", ".", "'", '"',"?", "!", ";"]
+const wordCounts = {}
+
+
+const cleanStr = function(str,specialChars){
+    let returnedString = str
+    for(let i in specialChars)
+        returnedString = returnedString.split(specialChars[i]).join(" ")
+    return returnedString
+}
+
+const addCreateCounter = function(word,wordCounts){
+        if(typeof wordCounts[word] != "undefined")
+            wordCounts[word]++
+        else
+            wordCounts[word] = 1
+}
+
+const countWords = function(str,wordCounts){
+    const arr = str.split(" ")
+    for(let word of arr){
+        addCreateCounter(word,wordCounts)
+    }
+}
+
+const fillWordCounts = function(str,specialChars,wordCounts){
+    let cleanString = cleanStr(str,specialChars)
+    countWords(cleanString,wordCounts)
+}
+
+fillWordCounts(story,specialChars,wordCounts)
+console.log(wordCounts)
